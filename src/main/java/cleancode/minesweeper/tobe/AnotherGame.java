3cf247b06da2dc05477a40f9f6d9db3f59b46a0b
bak3839 <bak3839@naver.com>
2025-05-26 14:39:35 +0900

    인터페이스 분리 원칙 - 클래스가 자신이 사용하지 않는 인터페이스를 의존하지 않도록 설계

---
+++ src/main/java/cleancode/minesweeper/tobe/AnotherGame.java
@@ -0,0 +1,11 @@
+package cleancode.minesweeper.tobe;
+
+import cleancode.minesweeper.tobe.game.GameRunnable;
+
+// run만 필요하다고 가정
+public class AnotherGame implements GameRunnable {
+    @Override
+    public void run() {
+        // 게임 진행
+    }
+}
