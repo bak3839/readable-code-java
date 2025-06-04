b2403f8058ed953647f0d61e282ba6759d80f02f
bak3839 <bak3839@naver.com>
2025-06-04 15:09:58 +0900

    StudyCafeSeatPass와 StudyCafeLockerPass를 가지는 Order객체 도출

---
+++ src/main/java/cleancode/studycafe/mytobe/order/StudyCafePassOrder.java
@@ -0,0 +1,18 @@
+package cleancode.studycafe.mytobe.order;
+
+import cleancode.studycafe.mytobe.pass.StudyCafeLockerPass;
+import cleancode.studycafe.mytobe.pass.StudyCafeSeatPass;
+
+public class StudyCafePassOrder {
+    private final StudyCafeSeatPass seatPass;
+    private final StudyCafeLockerPass lockerPass;
+
+    private StudyCafePassOrder(StudyCafeSeatPass seatPass, StudyCafeLockerPass lockerPass) {
+        this.seatPass = seatPass;
+        this.lockerPass = lockerPass;
+    }
+
+    public static StudyCafePassOrder of(StudyCafeSeatPass seatPass, StudyCafeLockerPass lockerPass) {
+        return new StudyCafePassOrder(seatPass, lockerPass);
+    }
+}
