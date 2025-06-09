97666286473c00ac244510a69eb5e160036edb74
bak3839 <bak3839@naver.com>
2025-06-09 17:28:21 +0900

    DisplayName과 메서드명 명확한 의도를 나타낼 수 있도록 수정

--- src/test/java/cleancode/studycafe/tobe/model/order/StudyCafePassOrderTest.java
+++ src/test/java/cleancode/studycafe/tobe/model/order/StudyCafePassOrderTest.java
@@ -17,7 +17,8 @@ class StudyCafePassOrderTest {
 
     /**
      * "2주권 이상은 10% 할인이 적용된다." -> 이름을 바꾸자
-     * 1. seatPassProvider 의존 -> 일급 컬렉션에서 duration == 2 인 이용권 불러와서 테스트
+     * 1. 일급 컬렉션에서 duration == 2 인 이용권 불러와서 테스트
+     *    - seatPassProvider 의존
      * 2. of 메서드를 통해 직접 객체를 생성하여 테스트
      *    - 생성 시 할인률을 0.1로 직접 설정하기 때문에 2주권 이상만 할인된다는 것이 명확하지 않음
      *
@@ -26,8 +27,8 @@ class StudyCafePassOrderTest {
      * - 할인률을 가지는 이용권들을 할인이 되는지 직관적으로 확인 가능
      */
     @Test
-    @DisplayName("할인이 적용되는 이용권의 총 결제 금액은 최종 금액에서 할인 금액이 차감된 값이어야 한다.")
-    void discountPass() {
+    @DisplayName("할인이 적용되는 이용권의 총 결제 금액은 이용권 가격에서 할인 가격이 차감된 값이어야 한다.")
+    void totalPriceIsPassPriceSubtractDiscountPrice() {
         // given
         StudyCafeSeatPass seatPass = StudyCafeSeatPass.of(WEEKLY, 2, 100000, 0.1);
         StudyCafePassOrder order = StudyCafePassOrder.of(seatPass, null);
@@ -40,7 +41,7 @@ class StudyCafePassOrderTest {
     }
 
     @Test
-    @DisplayName("사물함 이용 금액은 할인이 적용되지 않으며 총 결제 금액에 합산되어야 한다.")
+    @DisplayName("사물함 이용 가격은 할인이 적용되지 않으며 총 결제 금액에 합산되어야 한다.")
     void includeLockerPriceInTotalPriceWhenLockerPurchased() {
         // given
         StudyCafeSeatPass seatPass = StudyCafeSeatPass.of(WEEKLY, 4, 150000, 0.1);
